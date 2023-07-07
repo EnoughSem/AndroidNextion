6a3708c65088d66de443d6b3945435082bfa09d9
Владислав Евсеев <vladislaaw@mail.ru>
2023-07-07 16:20:44 +0300

    fix datetime

--- app/src/main/java/com/example/nextoinkotlin/AddReservActivity.kt
+++ app/src/main/java/com/example/nextoinkotlin/AddReservActivity.kt
@@ -19,13 +19,15 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.util.Date
 
 class AddReservActivity : AppCompatActivity() {
 
-    private lateinit var dateStart: Date
-    private lateinit var dateEnd: Date
+    private lateinit var dateStart: LocalDateTime
+    private lateinit var dateEnd: LocalDateTime
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,22 +45,22 @@ class AddReservActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private suspend fun addTime() {
+        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        setDateTime(dateFormat)
 
-        setDateTime()
-
-        val url = "https://mamont-server.ru:8888/api/schedule/788d3103-8a54-4aae-86fb-f19b5c09db58/$dateStart/$dateEnd"
+        val url =
+            "https://mamont-server.ru:8888/api/schedule/788d3103-8a54-4aae-86fb-f19b5c09db58/$dateStart/$dateEnd"
         HttpClient().use { client ->
             val date = client.get<String>(url)
             if (date == "[]") {
-                client.request("https://mamont-server.ru:8888/api/schedule"){
+                client.request("https://mamont-server.ru:8888/api/schedule") {
                     contentType(ContentType.Application.Json)
                     method = HttpMethod.Post
-                    body =  "{\"studio_id\": \"788d3103-8a54-4aae-86fb-f19b5c09db58\","+
+                    body = "{\"studio_id\": \"788d3103-8a54-4aae-86fb-f19b5c09db58\"," +
                             "\"start\": \"$dateStart\"," +
                             "\"end\": \"$dateEnd\"}"
                 }
-            }
-            else{
+            } else {
                 CoroutineScope(Dispatchers.Main).launch {
                     Toast.makeText(
                         applicationContext,
@@ -71,17 +73,20 @@ class AddReservActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun setDateTime(){
+    private fun setDateTime(dateFormat: DateTimeFormatter) {
         val editTextDate6 = findViewById<EditText>(R.id.editTextDate6)
         val editTextDate5 = findViewById<EditText>(R.id.editTextDate5)
         val editTextDate3 = findViewById<EditText>(R.id.editTextDate3)
         val editTextTime6 = findViewById<EditText>(R.id.editTextTime6)
         val editTextTime4 = findViewById<EditText>(R.id.editTextTime4)
 
-        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-        var dateStr =  editTextDate6.text.toString() + "-" + editTextDate5.text.toString() + "-" + editTextDate3.text +" "+
-                editTextTime6.text.toString() + ":" + editTextTime4.text.toString() +":00"
-        dateStart = Date.parse(dateStr, dateFormat)
+        var dateStr =
+            editTextDate6.text.toString().padStart(4, '0') + "-" + editTextDate5.text.toString()
+                .padStart(2, '0') + "-" +
+                    editTextDate3.text.toString()
+                        .padStart(2, '0') + " " + editTextTime6.text.toString().padStart(2, '0') +
+                    ":" + editTextTime4.text.toString().padStart(2, '0') + ":00"
+        dateStart = LocalDateTime.parse(dateStr, dateFormat)
 
 
         val editTextDate9 = findViewById<EditText>(R.id.editTextDate9)
@@ -90,9 +95,13 @@ class AddReservActivity : AppCompatActivity() {
         val editTextTime5 = findViewById<EditText>(R.id.editTextTime5)
         val editTextTime3 = findViewById<EditText>(R.id.editTextTime3)
 
-        dateStr =  editTextDate9.text.toString() + "-" + editTextDate8.text.toString() + "-" + editTextDate7.text +" "+
-                editTextTime5.text.toString() + ":" + editTextTime3.text.toString() +":00"
-        dateEnd = Date.parse(dateStr, dateFormat)
-
+        dateStr =
+            editTextDate9.text.toString().padStart(4, '0') + "-" + editTextDate8.text.toString()
+                .padStart(2, '0') + "-" +
+                    editTextDate7.text.toString()
+                        .padStart(2, '0') + " " + editTextTime5.text.toString()
+                .padStart(2, '0') + ":" +
+                    editTextTime3.text.toString().padStart(2, '0') + ":00"
+        dateEnd = LocalDateTime.parse(dateStr, dateFormat)
     }
 }
